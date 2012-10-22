a1bc7c39f2f2230aaf4283b69409b242ae4c8a92
cwebbdesign <christopherwdesign@gmail.com>
2012-10-22 18:01:53 +0200

    add rss alias, change bash color scheme, add solarized color scheme and pathogen bundle management for vim

--- install-deps.sh
+++ install-deps.sh
@@ -30,3 +30,14 @@ git clone git://github.com/dronir/SpotifyControl.git
 
 # add DropBox uploader
 git clone git://github.com/andreafabrizi/Dropbox-Uploader.git
+
+# Install pathogen
+# windows change /.vim to \vimfiles
+# https://github.com/tpope/vim-pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle;
+curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+# Solarized vim
+cd ~/.vim/bundle
+git clone git://github.com/altercation/vim-colors-solarized.git
